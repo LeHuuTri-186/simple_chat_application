5ff7517987c70719e62603a15133793e188c8654
LeHuuTri-186 <110376741+LeHuuTri-186@users.noreply.github.com>
2024-10-25 23:42:46 +0700

    Remove API key

--- lib/utils/api_key.dart
+++
@@ -1,2 +0,0 @@
-const OPENAI_API_KEY = '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';
-const GEMINI_API_KEY = "AIzaSyBI8W-oB0oFNYtxzHVEoze-cvm3QJ89nnk";
